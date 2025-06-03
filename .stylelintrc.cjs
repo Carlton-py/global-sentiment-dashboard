f6a8e896422846198d8b98a53cd38c3ecc680453
Carlton <gboxy619@yahoo.com>
2025-06-03 18:39:51 +0100

    Updates

---
+++ .stylelintrc.cjs
@@ -0,0 +1,18 @@
+// .stylelintrc.cjs
+module.exports = {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "tailwind",
+          "apply",
+          "variants",
+          "responsive",
+          "screen",
+        ],
+      },
+    ],
+  },
+};
